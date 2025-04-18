4600149955b11af453d66471f0d23249467f183b
KoudaAyu <k024g0061@g.neec.ac.jp>
2025-04-18 12:02:32 +0900

    4/18 進捗 完成

--- main.cpp
+++ main.cpp
@@ -16,21 +16,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
-	Vector3 translate{ 4.1f,2.6f,0.8f };
-	Vector3 scale{ 1.5f,5.2f,7.3f };
-	Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
-	Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
-	Vector3 point{ 2.3f,3.8f,1.4f };
-	Matrix4x4 transformMatrix = {
-	{
-		{1.0f, 2.0f, 3.0f, 4.0f},
-		{3.0f, 1.0f, 1.0f, 2.0f},
-		{1.0f, 4.0f, 2.0f, 3.0f},
-		{2.0f, 2.0f, 1.0f, 3.0f}
-	}
-	};
-
-	Vector3 transformd = VectorTransform(point, transformMatrix);
+	Vector3 rotate{ 0.4f,1.43f,-0.8f };
+	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+	Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
 ;
 
 	// ウィンドウの×ボタンが押されるまでループ
@@ -45,9 +35,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-		VectorScreenPrintf(0, 0, transformd, "transformd");
-		MatrixScreenPrintf(0, kRowHeight * 2, translateMatrix, "translateMatrix");
-		MatrixScreenPrintf(0, kRowHeight *2 * 4, scaleMatrix, "scaleMatrix");
+	
 
 		///
 		/// ↑更新処理ここまで
@@ -57,6 +45,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 		
+		MatrixScreenPrintf(0, 0, rotateXMatrix, "rotateXMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5, rotateYMatrix, "rotateYMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 10, rotateZMatrix, "rotateZMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 15, rotateXYZMatrix, "rotateXYZMatrix");
+
 		///
 		/// ↑描画処理ここまで
 		///
